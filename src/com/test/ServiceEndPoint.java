efcae13a351fe0ba7cbaf8f8a391a5c633e96e32
Vaibhav Rangare <47136076+VaibhavRangare@users.noreply.github.com>
2020-08-16 12:09:29 +0530

    Update ServiceEndPoint.java

--- src/com/test/ServiceEndPoint.java
+++ src/com/test/ServiceEndPoint.java
@@ -22,14 +22,11 @@ public class ServiceEndPoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/JSONEndpoint")
 	public JSONObject getMessage1(){
-		JSONObject obj = new JSONObject();
-
+	      JSONObject obj = new JSONObject();
 	      obj.put("name", "foo");
 	      obj.put("num", new Integer(100));
 	      obj.put("balance", new Double(1000.21));
 	      obj.put("is_vip", new Boolean(true));
-
-	      System.out.print(obj); 
 	      return obj;
 	}
 	
